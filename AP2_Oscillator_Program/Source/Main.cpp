bac436ec703336cf3e65d9f76a1fa7ff9f4301a1
Max Burnell <spacer.contact@gmail.com>
2025-02-20 19:39:24 +0000

    Final_Sine_Process

--- AP2_Oscillator_Program/Source/Main.cpp
+++ AP2_Oscillator_Program/Source/Main.cpp
@@ -6,8 +6,11 @@
   ==============================================================================
 */
 
+#define _USE_MATH_DEFINES
+
 #include <JuceHeader.h>
 #include <iostream>
+#include <math.h>
 
 //==============================================================================
 class SineOsc {
@@ -17,6 +20,12 @@ class SineOsc {
             mTs = 1 / sampleRate;
             mAmp = amplitude;
             mPhase = phase;
+        }
+        double process(double freq)
+        {
+            double newPhase = mPhase + freq * 2 * M_PI * mTs;
+            mPhase = fmod(newPhase, 2 * M_PI);
+            return mAmp * cos(mPhase);
         }
 
     private:
@@ -28,7 +37,6 @@ class SineOsc {
 
 int main (int argc, char* argv[])
 {
-
     using namespace juce;
 
     WavAudioFormat format;
@@ -40,6 +48,13 @@ int main (int argc, char* argv[])
     buffer.clear();
     float* WritePtr = buffer.getWritePointer(0);
 
+    SineOsc sine(numSamples, 0.5, 0);
+
+    //sine process
+    for (int i = 0; i < numSamples; i++)
+    {
+        WritePtr[i] = sine.process(100);
+    }
 
     // Write file
     juce::File output("C:\\Users\\maxbu\\Desktop\\osc_test.wav");
