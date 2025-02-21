811f41f068b089991a08b6c6dc6dd4ee1d95e575
Max Burnell <spacer.contact@gmail.com>
2025-02-21 13:59:05 +0000

    Added_Env_&_Square_Wave
    envelope class and square wave (from sines)

--- AP2_Oscillator_Program/Source/Main.cpp
+++ AP2_Oscillator_Program/Source/Main.cpp
@@ -34,6 +34,52 @@ class SineOsc {
         double mPhase;
 };
 
+class ADEnv {
+    public:
+        ADEnv(double sampleRate, double attackTime,
+            double decayTime, double peakVal)
+        {
+            mPeakVal = peakVal;
+            mAttackRate = peakVal / (attackTime * sampleRate);
+            mDecayRate = peakVal / (decayTime * sampleRate);
+            mSamples = (attackTime * sampleRate) + (decayTime * sampleRate);
+            mValue = 0;
+            mState = 0; //0 attack, 1 release, 2 finish
+        }
+        int numSamples()
+        {
+            return mSamples;
+        }
+        double process()
+        {
+            if (mState == 0)
+            {
+                mValue += mAttackRate;
+                if (mValue >= mPeakVal)
+                {
+                    mState = 1;
+                }
+            }
+            if (mState == 1)
+            {
+                mValue -= mDecayRate;
+                if (mValue <= 0)
+                {
+                    mState = 2;
+                }
+            }
+            return mValue;
+        }
+
+    private:
+        double mPeakVal;
+        double mAttackRate;
+        double mDecayRate;
+        double mValue;
+        int mState;
+        int mSamples;
+};
+
 
 int main (int argc, char* argv[])
 {
@@ -49,12 +95,27 @@ int main (int argc, char* argv[])
     float* WritePtr = buffer.getWritePointer(0);
 
     SineOsc sine(numSamples, 0.5, 0);
+    ADEnv env(numSamples, 0.1, 1, 1);
 
     //sine process
+        //squarewave
+    int nHarmonics = 10;
+    double nyquist = numSamples / 2;
+    std::vector<SineOsc> oscillators;
+    for (int i = 0; i < nHarmonics; i++)
+    {
+        oscillators.push_back(SineOsc(numSamples, 0.5, -M_PI / 2));
+    }
     for (int i = 0; i < numSamples; i++)
     {
-        WritePtr[i] = sine.process(100);
+        WritePtr[i] = 0;
+        for (int j = 1; j < nHarmonics; j++)
+        {
+            int h = 2 * j - 1;
+            WritePtr[i] = env.process() * (WritePtr[i] + (1.0 / h) * oscillators[j].process(h * 100.0));
+        }
     }
+
 
     // Write file
     juce::File output("C:\\Users\\maxbu\\Desktop\\osc_test.wav");
